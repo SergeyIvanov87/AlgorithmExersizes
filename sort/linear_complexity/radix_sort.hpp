def98f06d6d6579d1453f23367e70b03ce5c3840
SergeyIvanov87 <sergius140608@rambler.ru>
2021-05-26 23:33:04 +0300

    Implement radix sort

--- sort/linear_complexity/radix_sort.hpp
+++ sort/linear_complexity/radix_sort.hpp
@@ -9,10 +9,12 @@ void radix_sort(std::vector<T> &A, U max_digit_count, Tracer tracer)
 {
     std::vector<T> intermediate_array(A.size(), T{});
 
+    size_t exp = 1;
     for(U digit = 0; digit < max_digit_count; digit++)
     {
         tracer(digit);
-        countingsort(A, intermediate_array, size_t(10), tracer, (10 * digit + 1 ????? 10, 100, 1000));
+        countingsort(A, intermediate_array, size_t(10), tracer, exp);
+        exp *= 10;
 
         A.swap(intermediate_array);
         std::fill(intermediate_array.begin(), intermediate_array.end(), T{});
